df15c785fbd4998f40609ed0b8ba16517b86dfc2
JuanSulca <33933169+JuanSulca@users.noreply.github.com>
2018-07-15 13:25:28 -0500

    Programa inicial

---
+++ ProTank_v1/ProTank_v1/Model/Contrato.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProTank_v1
+{
+    class Contrato
+    {
+        public String idPer { set; get; }
+        public decimal monto { set; get; }
+        public int calificacion { set; get; }
+        public String idCon { set; get; }
+        public String descrip { set; get; }
+        public DateTime fechaI { set; get; }
+        public DateTime fechaE { set; get; }
+
+        public Contrato(String idPer, decimal monto, String idCon, DateTime fechaI)
+        {
+            this.idPer = idPer;
+            this.monto = monto;
+            this.idCon = idCon;
+            this.fechaI = fechaI;
+        }
+
+        public Contrato(String idPer, decimal monto, int calificacion, String idCon, String descrip, DateTime fechaI, DateTime fechaE)
+        {
+            this.idPer = idPer;
+            this.monto = monto;
+            this.calificacion = calificacion;
+            this.idCon = idCon;
+            this.descrip = descrip;
+            this.fechaI = fechaI;
+            this.fechaE = fechaE;
+        }
+
+        public Contrato()
+        {
+
+        }
+
+    }
+}
