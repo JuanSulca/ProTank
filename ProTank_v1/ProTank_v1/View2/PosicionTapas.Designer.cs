7cf02deea55d665a05addd26bb1268c035619fe8
Jose Luis Murillo <jose719810@hotmail.com>
2018-08-12 14:34:30 -0500

    labels en diseño

--- ProTank_v1/ProTank_v1/View2/PosicionTapas.Designer.cs
+++ ProTank_v1/ProTank_v1/View2/PosicionTapas.Designer.cs
@@ -37,6 +37,8 @@
             this.numericUpDownNumTapas = new System.Windows.Forms.NumericUpDown();
             this.numericUpDownVolTapas = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel17.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNumTapas)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownVolTapas)).BeginInit();
@@ -45,6 +47,8 @@
             // panel17
             // 
             this.panel17.BackColor = System.Drawing.Color.Silver;
+            this.panel17.Controls.Add(this.label3);
+            this.panel17.Controls.Add(this.label2);
             this.panel17.Controls.Add(this.button1);
             this.panel17.Controls.Add(this.numericUpDownVolTapas);
             this.panel17.Controls.Add(this.numericUpDownNumTapas);
@@ -57,7 +61,7 @@
             this.panel17.Location = new System.Drawing.Point(0, 0);
             this.panel17.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel17.Name = "panel17";
-            this.panel17.Size = new System.Drawing.Size(329, 248);
+            this.panel17.Size = new System.Drawing.Size(423, 248);
             this.panel17.TabIndex = 18;
             // 
             // label1
@@ -170,11 +174,29 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "[cm]";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(338, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "[litros]";
+            // 
             // PosicionTapas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(329, 248);
+            this.ClientSize = new System.Drawing.Size(423, 248);
             this.Controls.Add(this.panel17);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "PosicionTapas";
@@ -198,5 +220,7 @@
         private System.Windows.Forms.NumericUpDown numericUpDownVolTapas;
         private System.Windows.Forms.NumericUpDown numericUpDownNumTapas;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
